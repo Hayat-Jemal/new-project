8f71c7ed397144c678f4d72fd4a216040d9e0add
Hayat Jemal <htjemal2000@gmail.com>
2021-11-20 22:48:15 +0300

    update

--- src/Components/Home/HomeContent/HomeContent.js
+++ src/Components/Home/HomeContent/HomeContent.js
@@ -5,9 +5,8 @@ const HomeContent = () => {
       <div className="home__contents">
         <h1 className="home__title">Let's Shine their Ability</h1>
         <p className="home__description">
-          The water crisis is massive. But together, we can solve it. Join The
-          Spring, our community of monthly givers, to bring clean and safe water
-          to families around the world every single month.
+          Join us and help us to continue to be there for children with learning
+          disabilities in Ethiopia.
         </p>
       </div>
     );
