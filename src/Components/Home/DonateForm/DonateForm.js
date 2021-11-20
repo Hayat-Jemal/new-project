f9e8f37c417bb7e26777eed83d39eedf4962da22
Hayat Jemal <htjemal2000@gmail.com>
2021-11-20 23:38:39 +0300

    update

---
+++ src/Components/Home/DonateForm/DonateForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const DonateForm = () => {
+    return (
+        <div>
+            <div>
+                <button>GIVE ONCE</button>
+                <button>MONTHLY</button>
+            </div>
+            <div>
+                <form action="">
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default DonateForm
--- src/Components/Home/HomeContent/HomeContent.js
+++ src/Components/Home/HomeContent/HomeContent.js
@@ -3,7 +3,7 @@ import "./homeContent.css"
 const HomeContent = () => {
     return (
       <div className="home__contents">
-        <h1 className="home__title">Let's Shine their Ability</h1>
+        <p className="home__title">Let's Shine their Ability</p>
         <p className="home__description">
           Join us and help us to continue to be there for children with learning
           disabilities in Ethiopia.
