7a5e87f32e0a506e7772e1c427a3b033c09f09d9
Hayat Jemal <htjemal2000@gmail.com>
2021-11-20 23:47:19 +0300

    update

--- src/Components/Home/DonateForm/DonateForm.js
+++ src/Components/Home/DonateForm/DonateForm.js
@@ -2,18 +2,20 @@ import React from 'react'
 
 const DonateForm = () => {
     return (
+      <div>
         <div>
-            <div>
-                <button>GIVE ONCE</button>
-                <button>MONTHLY</button>
-            </div>
-            <div>
-                <form action="">
-
-                </form>
-            </div>
+          <button>GIVE ONCE</button>
+          <button>MONTHLY</button>
         </div>
-    )
+        <div>
+          <div className="form__header">Choose an amount to give per month</div>
+          <form action="">
+            <input type="text" placeholder="$10GBP/mo" />
+            <input type="text" name="" id="" placeholder="$20GBP/mo" />
+          </form>
+        </div>
+      </div>
+    );
 }
 
 export default DonateForm
