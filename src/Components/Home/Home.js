99b3d3a7fd4df8d9d142c9e9c2cc638ee7fa3438
Hayat Jemal <htjemal2000@gmail.com>
2021-11-21 21:32:05 +0300

    update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,9 @@ import DonateForm from "./DonateForm/DonateForm";
 // import { Link } from "react-router-dom";
 import "./home.css";
 import HomeContent from "./HomeContent/HomeContent";
-
+import image1 from "../Images/CP1.jpg"
+import image2 from "../Images/CP2.jpg";
+import image3 from "../Images/CP3.jpg"; 
 const Home = () => {
   return (
     <div className="home">
@@ -36,8 +38,22 @@ const Home = () => {
         <HomeContent />
         <DonateForm />
       </div>
-      <OurWork/>
-      {/* <About /> */}
+      <div className="" ></div>
+      <OurWork
+        image={image1}
+        title="School Breakfast"
+        description="Each school day, the School Breakfast Program provides a nutritious morning meal for 31 students."
+      />
+      <OurWork
+        image={image2}
+        title="RCA Technology Camp"
+        description="Raeye Children Aid (RCA) Technology Training teaches computer programming skills through fun and exciting ways. RCA Tech training is perfect for kids to expand their creativity and build confidence as they learn to problem-solve through coding."
+      />
+      <OurWork
+        image={image3}
+        title="RESPOND TO COVID-19"
+        description="We are responding to COVID-19 by feeding people in need. You can help.Donate"
+      />
     </div>
   );
 };
--- src/Components/OurWork/OurWork.js
+++ src/Components/OurWork/OurWork.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import "./ourWork.css";
-const OurWork = () => {
-    return (
+const OurWork = ({ image, title, description }) => {
+  return (
+    <div>
       <div className="ourWork">
         <p className="title">Our Work</p>
-        <p>
+        <p className="ourWork__goal">
           Our goal is to help the children develop the skills needed to succeed
           in school, obtain strong careers, and be active participants in their
           communities.
         </p>
       </div>
-    );
-}
+      <div>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
 
 export default OurWork;
