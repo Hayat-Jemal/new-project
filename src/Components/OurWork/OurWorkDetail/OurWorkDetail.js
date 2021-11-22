7d4e2493c206e317fc231d6cf0728a6e63817084
Hayat Jemal <htjemal2000@gmail.com>
2021-11-23 00:18:45 +0300

    update

--- src/Components/OurWork/OurWorkDetail/OurWorkDetail.js
+++ src/Components/OurWork/OurWorkDetail/OurWorkDetail.js
@@ -2,10 +2,12 @@ import React from 'react'
 import "./ourWorkDetail.css";
 const OurWorkDetail = ({ image, title, description }) => {
   return (
-    <div className="ourWorkDetail__contents">
+    <div className="ourWorkDetail__container">
       <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <div className="ourWorkDetail__content">
+        <h3 className="ourWorkDetail__title">{title}</h3>
+        <p className="ourWorkDetail__description">{description}</p>
+      </div>
     </div>
   );
 };
