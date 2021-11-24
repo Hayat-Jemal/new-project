1abb9dee957cc23a2f76ca89218cc50e3bb0041f
Hayat Jemal <htjemal2000@gmail.com>
2021-11-25 01:15:41 +0300

    update

--- src/Components/Footer/FooterLast/FooterLast.js
+++ src/Components/Footer/FooterLast/FooterLast.js
@@ -8,25 +8,32 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 const FooterLast = () => {
   return (
-    <div>
-      <div>
+    <div className="FooterLast">
+      <div className="FooterLast__first">
         <div className="footerLast__iconsWrapper">
           <div className="footerLast__icons">
-            <TwitterIcon />
-            <FacebookIcon />
-            <InstagramIcon />
-            <YouTubeIcon />
-            <LinkedInIcon />
+            <TwitterIcon className="icons" />
+            <FacebookIcon className="icons" />
+            <InstagramIcon className="icons" />
+            <YouTubeIcon className="icons" />
+            <LinkedInIcon className="icons" />
           </div>
-          <div>
-            privacy policy • made with{" "}
-            <FavoriteIcon style={{ fontSize: "15px" }} /> in nyc • get our
+          <div style={{ color: "#71716c" }}>
+            privacy policy   •   made with{" "}
+            <FavoriteIcon style={{ fontSize: "15px" }} /> in nyc   •    get our
             emails
           </div>
         </div>
-        {/* <div className="coun">unit</div> */}
+        <div className="footerLast__priceType">unit</div>
       </div>
-      <div>textbo</div>
+      <div className="footerLast__text">
+        © 2021 | 40 Worth St. NY, NY 10013 | 1 Devonshire St, London W1W 5DR |
+        Charity Global, Inc., a US 501 (c)(3) public charity, EIN 22-3936753 and
+        an ANBI in the Netherlands, RSIN 826151656 . Charity Global UK Limited
+        is a dual-qualified charity registered in England and Wales, charity
+        number 1169228. Charity Global UK Limited is wholly owned by Charity
+        Global, Inc.
+      </div>
     </div>
   );
 };
